6f49de9dc57fb9182ca60bcd0215b099829677d2
Luiz Ferreira <luixhferreira@gmail.com>
2026-09-10 12:59:06 -0300

    refactor(#28): show the summary with the definitive catalog codes and confidentiality levels
    Discipline and document type are displayed by code, areas by the seed names, the revision label comes from the integer version, and the confidentiality badge follows PUBLIC/CONFIDENTIAL/SECRET.

--- src/components/document-upload/ConfirmationStep.vue
+++ src/components/document-upload/ConfirmationStep.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { computed } from 'vue'
 import { useDocumentFormStore } from '../../stores/documentFormStore'
-import { findConfidentiality } from '../../utils/documentCatalog'
+import { findArea, findConfidentiality } from '../../utils/documentCatalog'
 import { formatDate } from '../../utils/formatters'
 import Badge from '../common/Badge.vue'
 import Button from '../common/Button.vue'
@@ -23,13 +23,14 @@ const code = computed(() => store.codePreview ?? EMPTY)
 const issueDate = computed(() => formatDate(issuedAt))
 const documentType = computed(() => {
   const type = store.selectedDocumentType
-  return type ? `${type.acronym} - ${type.name}` : EMPTY
+  return type ? `${type.code} - ${type.name}` : EMPTY
 })
 const discipline = computed(() => {
   const d = store.selectedDiscipline
-  return d ? `${d.acronym} - ${d.name}` : EMPTY
+  return d ? `${d.code} - ${d.name}` : EMPTY
 })
 const confidentiality = computed(() => findConfidentiality(store.form.confidentiality))
+const areas = computed(() => store.form.areas.map((code) => ({ code, name: findArea(code)?.name ?? code })))
 const missing = computed(() => Object.values(store.errors))
 const canPublish = computed(() => store.isValid && !store.publishing)
 </script>
@@ -45,7 +46,7 @@ const canPublish = computed(() => store.isValid && !store.publishing)
           <strong data-testid="document-code">{{ code }}</strong>
         </div>
         <div class="summary-cell summary-meta">
-          <KeyValue label="Versão atual" layout="row">{{ store.form.revision }}</KeyValue>
+          <KeyValue label="Versão atual" layout="row">{{ store.revision }}</KeyValue>
           <KeyValue label="Data emissão" layout="row">{{ issueDate }}</KeyValue>
           <KeyValue label="Responsável" layout="row">{{ store.form.author || EMPTY }}</KeyValue>
         </div>
@@ -60,7 +61,7 @@ const canPublish = computed(() => store.isValid && !store.publishing)
         </div>
         <div class="summary-cell">
           <KeyValue label="Confidencialidade">
-            <Badge v-if="confidentiality" :variant="confidentiality.value">{{ confidentiality.label }}</Badge>
+            <Badge v-if="confidentiality" :variant="confidentiality.value.toLowerCase()">{{ confidentiality.label }}</Badge>
             <template v-else>{{ EMPTY }}</template>
           </KeyValue>
         </div>
@@ -69,8 +70,8 @@ const canPublish = computed(() => store.isValid && !store.publishing)
       <div class="summary-row summary-row-two">
         <div class="summary-cell">
           <KeyValue label="Tags relacionadas">
-            <span v-if="store.form.areas.length" class="summary-tags">
-              <Badge v-for="area in store.form.areas" :key="area">{{ area }}</Badge>
+            <span v-if="areas.length" class="summary-tags">
+              <Badge v-for="area in areas" :key="area.code">{{ area.name }}</Badge>
             </span>
             <template v-else>{{ EMPTY }}</template>
           </KeyValue>
